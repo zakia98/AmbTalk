0dd96149d96c93a0be981d4933a82e8c4a02e0aa
Ameen <ameenahmed1998@gmail.com>
2022-04-04 21:37:57 +0100

    Clicking burger button will lead users to chat history

--- src/components/NavBar/BurgerMenu.js
+++ src/components/NavBar/BurgerMenu.js
@@ -1,10 +1,16 @@
 import React, { useState } from 'react';
 import burgericon from './menu_black_24dp.svg'
+import { useNavigate } from 'react-router-dom'
 import './BurgerMenu.css'
 export default function BurgerMenu(props) {
     
+    const navigate = useNavigate()
+
+    const handleClick = () => {
+        navigate('/chat-history')
+    }
 
     return(
-        <img className="burger-menu" src={burgericon}></img>
+        <img onClick={handleClick} className="burger-menu" src={burgericon}></img>
     )
 }  
