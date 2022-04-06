18ad97097b92d1c01b05c6327fe4b65909b20503
Ameen <ameenahmed1998@gmail.com>
2022-04-06 17:26:59 +0100

    Added an update which displayes the users id on login

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,16 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './NavBar.css'
 import BurgerMenu from './BurgerMenu';
 import Title from './TitleName';
-
+import { getAuth } from 'firebase/auth';
 
 export default function NavBar(props) {
     
+    const userid = props.currentUser ? props.currentUser.uid : null
 
     return(
         <div className='nav-bar'>
             <BurgerMenu></BurgerMenu>
             <Title currentChat='All Chats'></Title>
+            <p>{userid}</p>
+            
         </div>
     )
 }
