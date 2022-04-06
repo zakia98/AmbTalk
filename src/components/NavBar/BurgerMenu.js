40f0832f194241a7d053914a5391b5c611b46a05
Ameen <ameenahmed1998@gmail.com>
2022-04-06 17:27:42 +0100

    Changed burger menu naviate to main menu

--- src/components/NavBar/BurgerMenu.js
+++ src/components/NavBar/BurgerMenu.js
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import burgericon from './menu_black_24dp.svg'
 import { useNavigate } from 'react-router-dom'
 import './BurgerMenu.css'
+import {getAuth} from 'firebase/auth';
+
 export default function BurgerMenu(props) {
     
     const navigate = useNavigate()
 
     const handleClick = () => {
-        navigate('/chat-history')
+        navigate('/app')
     }
 
     return(
